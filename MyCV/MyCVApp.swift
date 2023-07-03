ae5f1c2addcbcc63b82096cc95fee600a87cb664
Rahul Bawane <rb.270995@gmail.com>
2023-07-03 21:00:13 +0530

    :tada: adding project to git

---
+++ MyCV/MyCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCVApp.swift
+//  MyCV
+//
+//  Created by Rahul Bawane on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
